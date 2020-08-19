af0448a0259aa90a315ddb1533fa0816105c87ea
cairumeng <cairumeng@gmail.com>
2020-08-19 11:10:48 +0200

    layouts

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            This projet wrritten by Laravel is a bulletin bord system dedicated to the sharing or exchange of messages
+            or other files on a network. -- Designed by Rumeng ❤
+        </p>
+
+        <p class="float-right"><a href="mailto:name@email.com">Contact us</a></p>
+    </div>
+</footer>
