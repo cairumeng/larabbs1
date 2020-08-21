2a1db0740657616246a7eb809a5151b9b81c7190
cairumeng <cairumeng@gmail.com>
2020-08-21 18:01:26 +0200

    topic create and show

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title','LaraBBS')</title>
-
+    <meta name="description" content="@yield('description','LaraBBS community')">
     <link rel="stylesheet" href="{{mix('css/app.css')}}">
-
 </head>
 
 <body>
--- resources/views/topics/_sidebar.blade.php
+++ resources/views/topics/_sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="card ">
     <div class="card-body">
-        <a href="" class="btn btn-success btn-block" aria-label="Left Align">
+        <a href="{{route('topics.create')}}" class="btn btn-success btn-block" aria-label="Left Align">
             <i class="fas fa-pencil-alt mr-2"></i>Create Topic</a>
     </div>
 </div>
